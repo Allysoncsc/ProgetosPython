a92b35ebd2c3588fedadf88696e2176076159fe6
AllysonCS <allysoncsc@gmail.com>
2022-08-22 17:35:13 -0300

    aula 109 composicao

---
+++ OO/composicao/mainComposicao.py
@@ -0,0 +1,19 @@
+from classesComp import Cliente, Endereco
+
+
+
+cliente1 = Cliente('Allyson',32)
+cliente1.insere_endereco('Fortaleza', 'CE')
+
+print(cliente1.nome)
+cliente1.lista_endereco()
+
+
+
+
+
+
+
+
+
+
