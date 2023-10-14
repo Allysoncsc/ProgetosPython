33d5ad89fbe36ef6234be0f5ee477deab73d6f5d
AllysonCS <allysoncsc@gmail.com>
2023-10-14 13:02:03 +0000

    stter

---
+++ OO/propertyStter.py
@@ -0,0 +1,19 @@
+
+class Caneta:
+    def __init__(self,cor):
+        self.cor= cor
+
+    def get_cor(self):
+        return self.cor
+
+
+    @property
+    def cor(self):
+        return self.cor
+
+
+    @setter
+    
+
+
+
