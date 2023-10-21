654f5573bca9e494759aebbe07c28377eb2734e7
unknown <allysoncsc@gmail.com>
2023-10-21 13:06:22 -0300

    aula 389, insert sqlit3

---
+++ OO1023/propertysetter.py
@@ -0,0 +1,22 @@
+
+class Connection:
+    def __init__(self, host = 'localhost'):
+        self.host = host
+        self.user = None
+        self.password = None
+        
+    def setuser(self, user):
+        self.user = user
+        
+    def setpassword(self, password):
+        self.password = password
+
+
+
+
+
+
+
+
+
+
--- sqlite/main.py
+++ sqlite/main.py
@@ -4,11 +4,32 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent
 DB_NAME = 'db.sqlite3'
 DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
 
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
-#SQL
+#criando tabela
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT, '
+    'name TEXT, '
+    'idade INTEGER'
+    ')'
+)
 
+#inserindo valores
+#assim está mais sujeito a SQL injection
+cursor.execute(
+    f'INSERT INTO {TABLE_NAME} ( name, idade) '
+    'VALUES ("Allyson Correia",33),'
+    '("goku",47),("vegeta",52),'
+    '("goha",26),("kuririn",47)'
+)
+connection.commit()
+
+
+#fechando conexão
 cursor.close()
 connection.close()
