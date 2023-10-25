b42c9ada7f1d71d6734b3a09ce8b45c460bc90ba
AllysonCS <allysoncsc@gmail.com>
2023-10-25 17:22:12 -0300

    aula 427, começando com django

---
+++ django_basico/blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.http import HttpResponse
+
+# from django.shortcuts import render
+
+
+def blog(request):
+    print("blog")
+    return HttpResponse("blog do app 1")
+
+
+def exemplo(request):
+    print("exemplo")
+    return HttpResponse("exemplo do app 1")
